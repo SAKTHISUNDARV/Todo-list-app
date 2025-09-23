e674828548d96d704ec160fc31da315e9aed5431
SAKTHISUNDARV <sundarsakthi450@gmail.com>
2025-09-23 09:25:29 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // Use environment variable for flexibility
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL ;
 const MAX_RETRIES = 2;
 
 const api = axios.create({
